111c4205f5b206bf367434c6fced26482258055b
nafaanandal <nafaanandalutfia@gmail.com>
2019-07-28 22:59:29 +0700

    add komentar

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// ----------------------functions----------------------
 function getData() {
     fetch('https://us-central1-si-xgracias.cloudfunctions.net/LihatNilaiMahasiswa')
         .then(res => {
+            // get data dari n-point trus ubah dalam bentuk json
             return res.json()
         })
         .then(data => {
@@ -8,12 +10,15 @@ function getData() {
             let idNilai
             let nilaiUTS
             let nilaiUAS
+            // looping data json
             data.forEach(item => {
+                // masukin data json ke variable yg baru dibuat
                 idFirebase = item.id
                 idNilai = item.id_nilai
                 nilaiUTS = item.nilaiUTS
                 nilaiUAS = item.nilaiUAS
             })
+            // masukin nilai dari variable ke element di HTML
             document.querySelector('#idFirebase').value = idFirebase
             document.querySelector('#idNilai').value = idNilai
             document.querySelector('#nilaiUTS').value = nilaiUTS
@@ -30,13 +35,16 @@ function updateData(id, idNilai, nilaiUTS, nilaiUAS) {
             'Content-Type': 'application/json'
         }
     }).then(() => {
+        // refresh halaman biar ke load data yang baru
         location.reload()
     })
 }
 
+// ----------------------main javascript----------------------
 getData()
 
 document.querySelector('#submitBtn').addEventListener('click', function () {
+    // get data yg baru dari element HTML utk di update
     let idData = document.querySelector('#idFirebase').value
     let elIDNilai = document.querySelector('#idNilai').value
     let elNnilaiUTS = document.querySelector('#nilaiUTS').value
